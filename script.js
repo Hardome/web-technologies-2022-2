53972792e09ff2826450b41b568360352fc856cd
Semyon Bedrin <semyonbedrin@gmail.com>
2023-03-02 16:27:13 +0500

    lesson 13 продолжил

--- script.js
+++ script.js
@@ -2,39 +2,65 @@ import {assortment} from "./pizza.js";
 import {toppings} from "./pizza.js";
 import {size} from "./pizza.js";
 
+let peperoni = document.getElementById("peperoni");
+let margarita = document.getElementById("margarita");
+let bavarian = document.getElementById("bavarian");
+let small = document.getElementById("smallButton");
+let big = document.getElementById("bigButton");
+let price = document.getElementById("price");
+let cheese = document.getElementById("cheese");
+let mozzarella = document.getElementById("mozzarella");
+let chedder = document.getElementById("chedder");
+
 let pizza = assortment.margarita;
-console.log(pizza.getStuffing()); //название пиццы
-console.log("размер " + pizza.getSize()); //размер базовой маргариты
-console.log("калории " + pizza.calculateCalories()); //каллории базовой маленькой маргариты
-console.log("цена " + pizza.calculatePrice()); //цена базовой маленькой маргариты
-console.log("");
 
-pizza.addTopping(toppings.cheese_board); //добавить сырный борт
-pizza.addTopping(toppings.cheddar_parmesan); //добавить чеддер
-pizza.addTopping(toppings.creamy_mozzarella); //добавить моцареллу
-console.log(pizza.getToppings()); //возвращает массив топингов
-console.log("калории "+pizza.calculateCalories()); //каллории базовой маленькой маргариты с топингами
-console.log("цена "+pizza.calculatePrice()); //цена базовой маленькой маргариты с топингами
+peperoni.onclick = function () {
+    pizza = assortment.pepperoni;
+}
 
-console.log("");
-pizza.setSize(size.big);
-console.log(pizza.getSize()); //размер  маргариты
-console.log("калории "+pizza.calculateCalories()); //каллории базовой большой маргариты с топингами
-console.log("цена "+pizza.calculatePrice()); //цена базовой большой маргариты с топингами
+margarita.onclick = function () {
+    pizza = assortment.margarita;
+}
 
-console.log("");
-pizza.removeTopping(toppings.cheddar_parmesan); //убрали чеддер
-console.log(pizza.getToppings()); //возвращает массив топингов
-console.log("калории "+pizza.calculateCalories()); //каллории базовой большой маргариты с топингами
-console.log("цена "+pizza.calculatePrice()); //цена базовой большой маргариты с топингами
+bavarian.onclick = function () {
+    pizza = assortment.bavarian;
+}
 
-console.log("");
-pizza.removeTopping(toppings.cheddar_parmesan); //убрали чеддер
-pizza.removeTopping(toppings.creamy_mozzarella); //убрали чеддер
-pizza.removeTopping(toppings.cheese_board); //убрали чеддер
-console.log(pizza.getToppings()); //возвращает массив топингов
-console.log("калории " + pizza.calculateCalories()); //каллории базовой большой маргариты
-console.log("цена " + pizza.calculatePrice()); //цена базовой большой маргариты
+small.onclick = function () {
+    pizza.setSize(size.small);
+}
+
+big.onclick = function () {
+    pizza.setSize(size.big);
+}
+
+cheese.onclick = function (){
+    if(pizza.getToppings().includes(toppings.cheese_board)){
+        pizza.removeTopping(toppings.cheese_board);
+    } else
+        pizza.addTopping(toppings.cheese_board);
+}
+
+mozzarella.onclick = function (){
+    if(pizza.getToppings().includes(toppings.creamy_mozzarella)){
+        pizza.removeTopping(toppings.creamy_mozzarella);
+    } else
+        pizza.addTopping(toppings.creamy_mozzarella);
+}
+
+chedder.onclick = function (){
+    if(pizza.getToppings().includes(toppings.cheddar_parmesan)){
+        pizza.removeTopping(toppings.cheddar_parmesan);
+    } else
+        pizza.addTopping(toppings.cheddar_parmesan);
+}
+
+price.onclick = function () {
+    price.textContent = "Цена "+pizza.calculatePrice()+" Каллории "+pizza.calculateCalories();
+/*    alert(pizza.getStuffing());
+    alert(pizza.getSize());*/
+}
+
 
 
 
