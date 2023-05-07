3eb84c28972a42e5309dfa0bdd1d5d3729047657
Semyon Bedrin <semyonbedrin@gmail.com>
2023-05-07 21:06:49 +0500

    1 to 3

--- index.php
+++ index.php
@@ -1,89 +1,52 @@
 <?php
 //1
-$a = -100;
-$b = -55;
+$i = 0;
+do {
+    if ($i == 0) {
+        echo $i . ' - это ноль.';
+    } else if ($i % 2 == 0) {
+        echo $i . ' - это четное число.';
+    } else {
+        echo $i . ' - это нечетное число.';
+    }
+    echo '<br>';
+    $i++;
+} while ($i <= 10);
 
-if ($a >= 0 && $b >= 0) {
-    echo $a - $b;
-} elseif ($a < 0 && $b < 0) {
-    echo $a * $b;
-} elseif (($a < 0 && $b >= 0) || ($a >= 0 && $b < 0)) {
-    echo $a + $b;
-}
+echo '<hr>';
 
 //2
-echo '<hr/>';
-$a = 14;
+$regions = [
+    'Московская область' => ['Москва', 'Зеленоград', 'Клин'],
+    'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт'],
+    'Рязанская область' => ['Рязань', 'Шацк', 'Ряжск', 'Тума', 'Касимов',]
+];
 
-switch ($a <= 15 && $a > 0) {
-    case true:
-        for (; $a <= 15; $a++) {
-            echo $a;
-        }
-        break;
-    case false:
-        return;
+foreach ($regions as $region => $cities) {
+    echo $region . ': <br>';
+    echo implode(', ', $cities) . '.';
+    echo '<br>';
 }
+
+echo '<hr>';
 
 //3
-$a = 5;
-$b = 10;
-echo '<hr/>';
-function addition($a, $b)
-{
-    return $a + $b;
+$letters = ['a' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e',
+    'ё' => 'yo', 'ж' => 'zh', 'з' => 'z', 'и' => 'i', 'й' => 'j', 'к' => 'k', 'л' => 'l', 'м' => 'm',
+    'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f',
+    'х' => 'x', 'ц' => 'cz', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shh', 'ъ' => '', 'ы' => 'y', 'ь' => '',
+    'э' => 'e', 'ю' => 'yu', 'я' => 'ya'];
+
+function transliterationToLatin($str, $letters) {
+    return str_replace(array_keys($letters), array_values($letters), strtolower($str));
 }
-
-function subtraction($a, $b)
-{
-    return $a - $b;
+function transliterationToKiril($str, $letters) {
+    return str_replace(array_values($letters), array_keys($letters), strtolower($str));
 }
-
-function multiplication($a, $b)
-{
-    return $a * $b;
-}
-
-function division($a, $b)
-{
-    return $a / $b;
-}
-
-echo 'Сложение ' . addition($a, $b) . '<br/>';
-echo 'Вычитание ' . subtraction($a, $b) . '<br/>';
-echo 'Умножение ' . multiplication($a, $b) . '<br/>';
-echo 'Деление ' . division($a, $b) . '<br/>';
-
-//4
-echo '<hr/>';
-function mathOperation($arg1, $arg2, $operation)
-{
-    switch ($operation) {
-        case 'addition':
-            return addition($arg1, $arg2);
-        case 'subtraction':
-            return subtraction($arg1, $arg2);
-        case 'multiplication':
-            return multiplication($arg1, $arg2);
-        case 'division':
-            return division($arg1, $arg2);
-    }
-}
-
-echo mathOperation($a, $b, 'addition');
-//6
-echo '<hr/>';
-function power($val, $pow) {
-    if ($pow == 1) {
-        return $val;
-    }
-    elseif ($pow > 0){
-        return  $val * power($val, $pow - 1);
-    }
-}
-
-echo power(2, 5);
-echo '<hr/>';
+echo transliterationToLatin('C кирилицы на латиницу', $letters);
+echo '<br>';
+echo transliterationToKiril('Naoborot', $letters);
+echo '<hr>';
 ?>
 
 <!doctype html>
@@ -93,7 +56,7 @@ echo '<hr/>';
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lesson17</title>
+    <title>Lesson18</title>
 </head>
 <body>
 <p><?php echo date('Y'); ?></p>
